8d6347b2b91f62f159f31df794f9ccd4e7b8b39f
julio-cesarcs <julio_cezzar@yahoo.com.br>
2023-12-09 10:35:41 -0300

    atualização do json

--- assets/script/api.js
+++ assets/script/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/julio-cesarcs/desafio-portfolio-dio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/julio-cesarcs/desafio-portfolio-dio/main/assets/data/profile.json'
     const fetchig = await fetch(url)
     return await fetchig.json()
 }
